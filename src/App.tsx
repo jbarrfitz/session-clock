22a751c4b2d5b0c923be17d9cc0edc4b3db4dad4
jbarrfitz <78324944+jbarrfitz@users.noreply.github.com>
2022-09-24 18:56:34 -0500

    initial commit; basic controls and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  ArrowDropUpSVGIcon,
+  ArrowDropDownSVGIcon,
+} from '@react-md/material-icons';
 
 function App() {
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [isCountingDown, setIsCountingDown] = useState(false);
+
+  const handleBreakIncrement = (e: React.MouseEvent<SVGElement>) => {
+    if (!isCountingDown) {
+      setBreakLength(breakLength + 1);
+    }
+  };
+
+  const handleBreakDecrement = (e: React.MouseEvent<SVGElement>) => {
+    if (!isCountingDown) {
+      setBreakLength(breakLength < 2 ? 1 : breakLength - 1);
+    }
+  };
+
+  const handleSessionIncrement = (e: React.MouseEvent<SVGElement>) => {
+    if (!isCountingDown) {
+      setSessionLength(sessionLength + 1);
+    }
+  };
+
+  const handleSessionDecrement = (e: React.MouseEvent<SVGElement>) => {
+    if (!isCountingDown) {
+      setSessionLength(sessionLength < 2 ? 1 : sessionLength - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Session Timer</h1>
+      <h4 id='break-label'>Break Length</h4>
+      <h4 id='session-label'>Session Length</h4>
+      <ArrowDropUpSVGIcon id='break-increment' onClick={handleBreakIncrement} />
+      <h3 id='break-length'>5</h3>
+      <ArrowDropDownSVGIcon
+        id='break-decrement'
+        onClick={handleBreakDecrement}
+      />
+      <ArrowDropUpSVGIcon
+        id='session-increment'
+        onClick={handleSessionIncrement}
+      />
+      <h3 id='session-length'>25</h3>
+      <ArrowDropDownSVGIcon
+        id='session-decrement'
+        onClick={handleSessionDecrement}
+      />
+      <h3>
+        <h2 id='timer-label'>Session</h2>
+        <h1 id='time-left'>25:00</h1>
+      </h3>
+      <button>Start/Stop</button>
+      <button>Reset</button>
     </div>
   );
 }
